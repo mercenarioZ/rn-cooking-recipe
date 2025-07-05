8b5afd49fc0d77b23dcd72dd49afbea463744583
mercenarioz <funnystar320@gmail.com>
2025-07-05 21:45:17 +0700

    fix(type): fix the weird type error

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import { ENV } from "./env";
-import { drizzle } from "drizzle-orm/singlestore/driver";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "../db/schema";
 
 const sql = neon(ENV.DB_URL);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { ENV } from "./config/env.js"; // This will now resolve to env.ts
+import { ENV } from "./config/env";
+import { db } from "./config/db";
+import { favoritesTable } from "./db/schema";
 
 const app = express();
 const PORT = ENV.PORT || 8080;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 app.get("/api/checkhealth", (_req, res) => {
   res.status(200).json({
@@ -10,6 +15,43 @@ app.get("/api/checkhealth", (_req, res) => {
     success: true,
   });
 });
+
+// favorites endpoint
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+
+    // Validate input
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({
+        error: "Missing required fields!",
+      });
+    }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json({
+      message: "Favorite added successfully",
+      favorite: newFavorite[0],
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to add favorite",
+    });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
