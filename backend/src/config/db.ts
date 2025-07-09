57c0ba8cb502b1c667252af5b782303c285aca2a
mercenarioZ <funnystar320@gmail.com>
2025-07-09 22:56:31 +0700

    roll everything back to js

--- backend/src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { ENV } from "./env";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "../db/schema";
-
-const sql = neon(ENV.DB_URL);
-
-export const db = drizzle(sql, { schema });
