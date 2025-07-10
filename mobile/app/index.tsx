923a74b398d8bfeae39aca558f7af1ba3113c433
mercenarioZ <funnystar320@gmail.com>
2025-07-11 00:55:22 +0700

    chore(ui): rn overview, add some assets

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ marginTop: 10 }}
+        onPress={() => alert("Button Pressed!")}
+      >
+        <Text>Click me</Text>
+      </TouchableOpacity>
+
+      <Link href="/about">Go to About</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ddd",
+  },
+
+  text: {
+    color: "blue",
+  },
+});
